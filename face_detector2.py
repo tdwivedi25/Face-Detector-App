c78ae993536f4bdbc680f983235c58a818eb376d
Pankaj <sdwivedi05@gmail.com>
2024-03-16 00:49:00 +0530

    first commit

---
+++ face_detector2.py
@@ -0,0 +1,26 @@
+import cv2
+
+face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml") #Stores the xmal file
+
+img=cv2.imread('news.jpg')
+gray_img=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # COLOR_BGR2GRAY converts the image to grayscale image
+
+faces=face_cascade.detectMultiScale(gray_img, #detectMultiScale uses the xml file to detect the face
+scaleFactor=1.2, # Narrows the image for better accuracy of detecting objects
+minNeighbors=5)
+
+for x, y,w, h in faces: #Creates the rectangle around the face
+   img=cv2.rectangle(img, (x,y), (x+w,y+h),(0,255,0),3) #4 paramters: image, point1, point2, rectangle color, thickness
+
+print(type(faces)) #type: array
+print(faces) #Tells the point "face" starts and length
+
+resized=cv2.resize(img, (int(img.shape[0]/3), int(img.shape[1]/3)))
+
+cv2.imshow("Gray",resized)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
